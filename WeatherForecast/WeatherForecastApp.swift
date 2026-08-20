199492a2066c79a7324700e4ed5cf37c9068bfa5
kelvin0722 <kelvinm442@gmail.com>
2026-08-20 11:11:50 +0300

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Kelvin Ngacha on 20/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
